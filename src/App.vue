faaf2c75e197a50ac49d96b399260870691f5e0f
Xinlai Fang <xfan0059@student.monash.edu>
2025-08-25 19:38:19 +1000

    router setting finished

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,46 @@
 <script setup>
-import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
-import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
-  <header>
-    <BHeader />
-  </header>
+  <BHeader />
 
-  <main>
-    <LibraryRegistrationForm />
-    <!-- <JSONLab /> -->
+  <main class="app-container container my-5">
+    <router-view />
   </main>
 </template>
 
-<style scoped>
-/* header {
-  line-height: 1.5;
+<style>
+
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+.app-container {
+  max-width: 80vw;
+  margin: 0 auto;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
+input.form-control:focus,
+textarea.form-control:focus,
+select.form-select:focus {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0,0,0,.1);
+  outline: 0;
+}
 </style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <header class="text-center mb-4">
+        <h1 class="display-5 fw-semibold">About Our Library</h1>
+      </header>
+      <p class="lead">
+        Welcome to our digital library! We're dedicated to providing a vast collection of
+        books and resources to our community.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
